558afe6f47ade664a42548814957bcab51e1037a
florent.meyniel <florent.meyniel@gmail.com>
2015-12-17 17:06:33 +0100

    add more options, preserving backward compatibility

--- fmspm12batch_GetClusterCoord.m
+++ fmspm12batch_GetClusterCoord.m
@@ -3,14 +3,24 @@ function [ClusterCoord_vx, xSPM] = fmspm12batch_GetClusterCoord(Peakmm, xSPM)
 % cluster corresponding to the input coordinate (in mm), and in the SPM.mat
 % selected interactively with its statistical threshold.
 % NB: Take the coordinate from the table, or from the Result panel, but not
-% the local maximal printed in the command line (which does not correspond 
-% to the actual grid of voxels). 
-% 
+% the local maximal printed in the command line (which does not correspond
+% to the actual grid of voxels).
+%
 % Usage:
 % [ClusterCoord_vx, xSPM] = fmspm12batch_GetClusterCoord(Peakmm)
+% NB: an extra argument xSPM can be passed in:
+%   - it can the output from a previous call of fmspm12batch_GetClusterCoord
+%     to avoid entering again which model, stat, ... should be used
+%     (typically, if multiple ROI are defined for the same model)
+%   - it can be a xSPM with the following field (easy to batch):
+%       .swd: full path of the SPM.mat
+%       .Ic: number of the contrast (cf. ordering of spmT maps)
+%       .k: minimum extend of clusters
+%       .u p-value to threshold the map
+%       .thresDesc: type of stat: 'FWE', 'FDR' or 'none'
 
 % initialize
-try ver = spm('Version'); 
+try ver = spm('Version');
 catch
     error('the SPM toolbox is not in the Matlab''s path')
 end
@@ -21,6 +31,25 @@ spm_results_ui('clear')
 % Get the xSPM, let the user select the contrast and statistical threshold.
 if nargin == 1
     [~, xSPM] = spm_getSPM;
+else
+    if ~isfield(xSPM, 'title')
+        % this means that xSPM was not computed automatically by SPM,
+        % but instead provided with minimal information by the user.
+        
+        % complete some fields with default settings
+        xSPM.n  = 1;  % value >1 are used to compute conjunctions
+        xSPM.Im = []; % empty because there is no masking
+        xSPM.pm = []; % no masking = no p-value to define the mask
+        xSPM.Ex = []; % irrelevant flag when there is no masking
+        
+        % get the contrast title field
+        SPM = load([xSPM.swd, '/SPM.mat']);
+        SPM = SPM.SPM;
+        xSPM.title = SPM.xCon(xSPM.Ic).name;
+        
+        % let SPM finish the job!
+        [~, xSPM] = spm_getSPM(xSPM);
+    end
 end
 
 % List all the cluster in this thresholded map
@@ -28,8 +57,8 @@ clusters = spm_clusters(xSPM.XYZ);
 
 % Get the voxel, in the list of coordinate, corresponding to a peak.
 % NB: Take the coordinate from the table, or from the Result panel, but not
-% the local maximal printed in the command line (which does not correspond 
-% to the actual grid of voxels). 
+% the local maximal printed in the command line (which does not correspond
+% to the actual grid of voxels).
 peakloc = ismember(xSPM.XYZmm', Peakmm,'rows');
 
 % check that there is a unique location
--- fmspm12batch_preproc_GetSliceTiming_NS.m
+++ fmspm12batch_preproc_GetSliceTiming_NS.m
@@ -1,15 +1,20 @@
-function [SliceTiming, TR, TE, SliceThickness, SpacingBetweenSlices] ...
-    = fmspm12batch_preproc_GetSliceTiming_NS(iSub, funcdir, datadir, spm_path)
+function [SliceTiming, TR, TE, SliceThickness, SpacingBetweenSlices, NumberOfSlices] ...
+    = fmspm12batch_preproc_GetSliceTiming_NS(iSub, funcdir, datadir, spm_path, ForceToThisDICOM)
 % Function to get (and save) the slice timing info from the Neurospin 
 % server for a particular subject. The timing is read in the header of a 
 % parciular dicom volume; it should not vary significantly across volume 
 % and subject (the accuracy of these numbers is <5ms).
+
+% The 5th argument is optinal. If provided, it should be the full path name
+% of the DICOM file serving as reference.
 
 % add spm in the path
 addpath(spm_path)
 
 % get the directory of imported data for this subject
 subjdir = sprintf('%s/subj%02.0f/%s/', datadir, iSub, funcdir);
+
+if nargin < 5
 
 % get subject NIP and date
 fname = spm_select('List', subjdir, '^B0_1_.*\.nii');
@@ -18,7 +23,13 @@ subID = fname(ind_(2)+1:ind_(3)-1);
 subDate = fname(ind_(3)+1:ind_(4)-1);
 
 % go into folder on the neurospin server
-base_ns = ['/neurospin/acquisition/database/TrioTim/', subDate];
+if str2double(subDate(1:4)) > 2015 || (str2double(subDate(1:4)) == 2015 && str2double(subDate(1:4))>= 11)
+    % data recorder after 2015/11 are on the Prisma repository
+    base_ns = ['/neurospin/acquisition/database/Prisma_fit/', subDate];
+else
+    % data recorder before 2015/11 are on the Trio repository
+    base_ns = ['/neurospin/acquisition/database/TrioTim/', subDate];
+end
 dname = dir([base_ns, '/', subID, '*']);
 dname = dname.name;
 subjdir_ns = [base_ns, '/', dname];
@@ -44,17 +55,23 @@ flist = dir([sess_dir_ns, '*.dcm']);
 
 hdr = spm_dicom_headers([sess_dir_ns, flist(2).name]);
 
+else
+    % FORCE TO TAKE THIS DICOM INTO ACCOUNT
+    hdr = spm_dicom_headers(ForceToThisDICOM);
+end
+
 % Get the timing info: 
 SliceTiming = hdr{1}.Private_0019_1029; % slice timing
 TR = hdr{1}.RepetitionTime/1000; % Convert to seconds
 TE = hdr{1}.EchoTime;            % leave in ms.
 SliceThickness = hdr{1}.SliceThickness;
 SpacingBetweenSlices = hdr{1}.SpacingBetweenSlices;
+NumberOfSlices = hdr{1}.Private_0019_100a;
 
 % save the sclice timing info
 fname = [subjdir, 'SliceTimingInfo.mat'];
 if exist(fname) 
     delete(fname); fprintf('\nremove previous SliceTimingInfo.mat\n')
 end
-save(fname, 'SliceTiming', 'TR', 'TE', 'SliceThickness', 'SpacingBetweenSlices')
+save(fname, 'SliceTiming', 'TR', 'TE', 'SliceThickness', 'SpacingBetweenSlices', 'NumberOfSlices')
 
